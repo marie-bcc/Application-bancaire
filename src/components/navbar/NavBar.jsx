1d474875f7c48695052e38f61ff953f29f0cd424
marie <127097512+marie-bcc@users.noreply.github.com>
2023-10-06 17:15:30 +0200

    Update logout

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,11 +1,21 @@
 import React from "react";
-import "./Navbar.css";
-import logo from "../../img/argentBankLogo.png";
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
-import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from "react-redux";
+import { logoutAsync } from "../../Features/auth/authActions";
+import { selectToken } from "../../Features/auth/authSlice";
+import "./Navbar.css";
+import logo from "../../img/argentBankLogo.png";
 
 function NavBar() {
+  const dispatch = useDispatch();
+  const token = useSelector(selectToken);
+
+  const handleLogoutClick = () => {
+    dispatch(logoutAsync());
+  };
+
   return (
     <nav className="main-nav">
       <Link className="main-nav-logo" to="/">
@@ -13,12 +23,19 @@ function NavBar() {
         <h1 className="sr-only">Argent Bank</h1>
       </Link>
       <div>
-        <Link className="main-nav-item" to="/login">
-          <FontAwesomeIcon
-          className="main-nav-item"
-          icon={faUserCircle} />
-          Sign In
-        </Link>
+        {token ? (
+          <Link className="main-nav-item" to="/" onClick={handleLogoutClick}>
+            
+            <FontAwesomeIcon className="main-nav-item" icon={faUserCircle} />
+            {/* <p>Tony </p> */}
+            Sign Out
+          </Link>
+        ) : (
+          <Link className="main-nav-item" to="/login">
+            <FontAwesomeIcon className="main-nav-item" icon={faUserCircle} />
+            Sign In
+          </Link>
+        )}
       </div>
     </nav>
   );
--- src/pages/UserProfile/UserProfile.jsx
+++ src/pages/UserProfile/UserProfile.jsx
@@ -1,44 +1,15 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { logoutAsync } from "../../Features/auth/authActions"; 
-import logo from "../../img/argentBankLogo.png";
 import "./UserProfile.css";
-
+import AccountContent from "./account/AccountContent";
+import Footer from "../../components/footer/Footer";
+import NavBar from "../../components/navbar/NavBar";
 
 function UserProfile() {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
-  const handleLogout = () => {
-    dispatch(logoutAsync()); 
-    navigate("/HomePage"); 
-  };
-
   return (
-    <> 
-      <nav className="main-nav">
-        <Link className="main-nav-logo" to="/">
-          <img
-            className="main-nav-logo-image"
-            src={logo}
-            alt="Argent Bank Logo"
-          />
-          <h1 className="sr-only">Argent Bank</h1>
-        </Link>
-        <div>
-          <Link className="main-nav-item" to="/user">
-            <i className="fa fa-user-circle"></i>
-            
-          </Link>
-          <Link className="main-nav-item" to="/">
-          <button onClick={handleLogout} className="main-nav-item">
-            <i className="fa fa-sign-out"></i>
-            Sign Out
-          </button>
-          </Link>
-        </div>
-      </nav>
+    <>
+      <NavBar />
+      <AccountContent />
+      <Footer />
     </>
   );
 }
